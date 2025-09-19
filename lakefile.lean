16f79d70a44ea3623483b4e0f81a0687f2c0c58b
Michael R Douglas <mdouglas@scgp.stonybrook.edu>
2025-09-19 12:01:26 -0400

    lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -20,3 +20,7 @@ require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
 meta if get_config? env = some "dev" then
 require «doc-gen4» from git
   "https://github.com/leanprover/doc-gen4" @ "main"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
